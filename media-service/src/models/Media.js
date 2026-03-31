ed70983aef8c83ef83855a23ed92c6c4527fcfc8
manohar12-max <2898manohar@gmail.com>
2026-03-31 18:31:41 +0530

    change

--- media-service/src/models/Media.js
+++ media-service/src/models/Media.js
@@ -28,6 +28,8 @@ const mediaSchema=new mongoose.Schema({
 }
 )
 mediaSchema.index({ userId: 1 }, { unique: true }) // This index will ensure that each user has only one media
+mediaSchema.index({ publicId: 1 }, { unique: true }) // This index will ensure that each publicId is unique
+mediaSchema.index({ userId: 1, publicId: 1 }, { unique: true }) // This index will ensure that each user has only one media with the same publicId
 const Media=mongoose.model('Media',mediaSchema);
 module.exports=Media;
 
